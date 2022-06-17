d6002f02f591ab27b86e515c7a10dbcf10fb2ba2
Zed <emailzed03@gmail.com>
2022-06-17 17:19:57 +1000

    rename

--- app/src/main/java/OwlVoiceAssistant/App.java
+++ app/src/main/java/OwlVoiceAssistant/App.java
@@ -2,10 +2,17 @@ package OwlVoiceAssistant;
 
 import OwlVoiceAssistant.Commands.CommandInterface;
 import OwlVoiceAssistant.Commands.MusicCommand;
+import OwlVoiceAssistant.TextToIntent.Intent;
+import OwlVoiceAssistant.TextToIntent.TTI;
+import ai.picovoice.cheetah.Cheetah;
+import ai.picovoice.cheetah.CheetahException;
+import ai.picovoice.cheetah.CheetahTranscript;
 import ai.picovoice.picovoice.Picovoice;
 import ai.picovoice.picovoice.PicovoiceException;
 import ai.picovoice.picovoice.PicovoiceInferenceCallback;
 import ai.picovoice.picovoice.PicovoiceWakeWordCallback;
+import ai.picovoice.porcupine.Porcupine;
+import ai.picovoice.porcupine.PorcupineException;
 import org.apache.log4j.PropertyConfigurator;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -23,6 +30,7 @@ public class App {
 
     private static final Logger logger = LogManager.getLogger(App.class);
     private TTS _tts;
+    private TTI _tti;
 
     private Map<String, CommandInterface> intentMap;
 
@@ -30,74 +38,92 @@ public class App {
      * Initialize the text to speech class
      * if this fails to initialize then the application will exit with ExitCode = 1
      */
-    public void InitializeTTS() {
+    private TTS InitializeTTS() {
         var voice = "dfki-prudence-hsmm";
-        _tts = new TTS(voice);
+        var tts = new TTS(voice);
         try {
-            _tts.Configure();
+            tts.Configure();
             logger.info("Initialized Text to speech with {}", voice);
         } catch (Exception e) {
             logger.fatal("Failed to initialize the text to speech object: exception = {}", e.getMessage());
             System.exit(1);
         }
+        return tts;
+    }
+
+    private TTI InitializeTTI(String grammarPath) {
+        return new TTI(grammarPath);
+    }
+
+    private void HandleIntent(Intent intent) {
+        String speak = this.intentMap.get(intent.intent).ExecuteCommand(intent);
     }
 
 
-    public void Run (String rhinoPath, String porcupinePath, String picovoiceKey) throws PicovoiceException, LineUnavailableException {
-        InitializeTTS();
+    public void Run (Properties prop) throws PicovoiceException, LineUnavailableException, PorcupineException, CheetahException {
+        _tts = InitializeTTS();
+        _tti = InitializeTTI(prop.getProperty("commandJson"));
+
+        String rhinoPath = prop.getProperty("rhinoPath");
+        String porcupinePath = prop.getProperty("porcupinePath");
+        String picovoiceKey = prop.getProperty("picovoiceKey");
 
         this.intentMap = GenerateIntentCommandMap.MapCommands();
 
-        PicovoiceWakeWordCallback wakeWordCallback = () -> {
-            System.out.println("Wake word detected!");
-            // let user know wake word was detected
-            //TODO: light up eyes and play notification sound?
-
-            // pause music is it's currently playing
-            if (MusicCommand.CurrentlyPlaying) {
-                MusicCommand.Pause();
-            }
-
-        };
-
-        PicovoiceInferenceCallback inferenceCallback = inference -> {
-            if (inference.getIsUnderstood()) {
-                final String intent = inference.getIntent();
-                final Map<String, String> slots = inference.getSlots();
-
-//                System.out.println(intent);
-//                slots.forEach((key, value) -> System.out.println("\t" + key + ":" + value));
-
-                String speak = this.intentMap.get(intent).ExecuteCommand(intent, slots);
-                if(speak == null) {
-                    logger.error("No mapping for the intent: {}, was found in the intentCommandMap. Did you add it in GenerateIntentCommandMap.MapCommands", intent);
-                    _tts.Speak("Sorry i could not find a command for the intent " + intent);
-                } else {
-                    _tts.Speak(speak);
-                }
-
-            } else {
-                _tts.Speak("I have no idea what you want me to do!");
-            }
-        };
+//        PicovoiceWakeWordCallback wakeWordCallback = () -> {
+//            System.out.println("Wake word detected!");
+//            // let user know wake word was detected
+//            //TODO: light up eyes and play notification sound?
+//
+//            // pause music is it's currently playing
+//            if (MusicCommand.CurrentlyPlaying) {
+//                MusicCommand.Pause();
+//            }
+//
+//        };
+//
+//        PicovoiceInferenceCallback inferenceCallback = inference -> {
+//            if (inference.getIsUnderstood()) {
+//                final String intent = inference.getIntent();
+//                final Map<String, String> slots = inference.getSlots();
+//
+//                String speak = this.intentMap.get(intent).ExecuteCommand(intent, slots);
+//                if(speak == null) {
+//                    logger.error("No mapping for the intent: {}, was found in the intentCommandMap. Did you add it in GenerateIntentCommandMap.MapCommands", intent);
+//                    _tts.Speak("Sorry i could not find a command for the intent " + intent);
+//                } else {
+//                    _tts.Speak(speak);
+//                }
+//
+//            } else {
+//                _tts.Speak("Sorry i didnt understand that");
+//            }
+//        };
 
 
-        Picovoice picovoice;
-        picovoice = new Picovoice.Builder()
+        Porcupine porcupine = new Porcupine.Builder()
                 .setAccessKey(picovoiceKey)
-                .setKeywordPath(porcupinePath)
-                .setWakeWordCallback(wakeWordCallback)
-                .setContextPath(rhinoPath)
-                .setInferenceCallback(inferenceCallback)
-                .setPorcupineSensitivity(0.7f)
+                .setBuiltInKeyword(Porcupine.BuiltInKeyword.COMPUTER)
                 .build();
 
-        logger.info("Initialized Picovoice");
+        Cheetah cheetah = new Cheetah.Builder()
+                .setAccessKey(picovoiceKey)
+                .setLibraryPath(Cheetah.LIBRARY_PATH)
+                .setModelPath(Cheetah.MODEL_PATH)
+                .build();
 
-        // buffers for processing audio
-        short[] picovoiceBuffer = new short[picovoice.getFrameLength()];
-        ByteBuffer captureBuffer = ByteBuffer.allocate(picovoice.getFrameLength() * 2);
-        captureBuffer.order(ByteOrder.LITTLE_ENDIAN);
+//        Picovoice picovoice;
+//        picovoice = new Picovoice.Builder()
+//                .setAccessKey(picovoiceKey)
+//                .setKeywordPath(porcupinePath)
+////                .setWakeWordCallback(wakeWordCallback)
+////                .setContextPath(rhinoPath)
+////                .setInferenceCallback(inferenceCallback)
+//                .setPorcupineSensitivity(0.7f)
+//                .build();
+
+//        logger.info("Initialized Picovoice");
+
 
         // get default audio capture device
         AudioFormat format = new AudioFormat(16000f, 16, 1, true, false);
@@ -110,51 +136,119 @@ public class App {
         // start audio capture
         micDataLine.start();
 
+        // buffers for processing audio
+        int frameLength = porcupine.getFrameLength();
+        ByteBuffer captureBuffer = ByteBuffer.allocate(frameLength * 2);
+        captureBuffer.order(ByteOrder.LITTLE_ENDIAN);
+        short[] audioBuffer = new short[frameLength];
+
         int numBytesRead;
-        boolean recordingCancelled = false;
-        while (!recordingCancelled) {
+        boolean awoken = false;
+        while (true) {
 
             // read a buffer of audio
             numBytesRead = micDataLine.read(captureBuffer.array(), 0, captureBuffer.capacity());
+//            totalBytesCaptured += numBytesRead;
 
-            // don't pass to Picovoice if we don't have a full buffer
-            if (numBytesRead != picovoice.getFrameLength() * 2) {
+            // don't pass to porcupine if we don't have a full buffer
+            if (numBytesRead != frameLength * 2) {
                 continue;
             }
 
             // copy into 16-bit buffer
-            captureBuffer.asShortBuffer().get(picovoiceBuffer);
+            captureBuffer.asShortBuffer().get(audioBuffer);
 
-            // process with picovoice
-            picovoice.process(picovoiceBuffer);
+            // process with porcupine
+            int result = porcupine.process(audioBuffer);
+            if (result >= 0) {
+                System.out.println("Computer wake word detected");
+                awoken = true;
+            }
+
+            if(awoken) {
+                CheetahTranscript transcriptObj = cheetah.process(audioBuffer);
+                System.out.print(transcriptObj.getTranscript());
+                if (transcriptObj.getIsEndpoint()) {
+                    CheetahTranscript endpointTranscriptObj = cheetah.flush();
+//                    System.out.println(endpointTranscriptObj.getTranscript());
+                    var intent = _tti.ParseTextToCommand(endpointTranscriptObj.getTranscript());
+
+                    awoken = false;
+                }
+                System.out.flush();
+            }
+
+
         }
+//
+//        // buffers for processing audio
+//        short[] picovoiceBuffer = new short[picovoice.getFrameLength()];
+//        ByteBuffer captureBuffer = ByteBuffer.allocate(picovoice.getFrameLength() * 2);
+//        captureBuffer.order(ByteOrder.LITTLE_ENDIAN);
+//
+//        // get default audio capture device
+//        AudioFormat format = new AudioFormat(16000f, 16, 1, true, false);
+//        DataLine.Info dataLineInfo = new DataLine.Info(TargetDataLine.class, format);
+//        TargetDataLine micDataLine;
+//
+//        micDataLine = (TargetDataLine) AudioSystem.getLine(dataLineInfo);
+//        micDataLine.open(format);
+//
+//        // start audio capture
+//        micDataLine.start();
+//
+//        int numBytesRead;
+//        boolean recordingCancelled = false;
+//        while (!recordingCancelled) {
+//
+//            // read a buffer of audio
+//            numBytesRead = micDataLine.read(captureBuffer.array(), 0, captureBuffer.capacity());
+//
+//            // don't pass to Picovoice if we don't have a full buffer
+//            if (numBytesRead != picovoice.getFrameLength() * 2) {
+//                continue;
+//            }
+//
+//            // copy into 16-bit buffer
+//            captureBuffer.asShortBuffer().get(picovoiceBuffer);
+//
+//            // process with picovoice
+//            picovoice.process(picovoiceBuffer);
+//        }
     }
 
 
     public static void main(String[] args) {
-        // print the default  wake commands that can be added
-//        System.out.println("Default wake commands");
-//        Porcupine.BUILT_IN_KEYWORD_PATHS.forEach((key,value) -> System.out.println(key + ":" + value));
-//        System.out.println("-----\n");
-
         if(args.length != 1) {
             System.out.println("Path to properties file is required");
             return;
         }
 
         PropertyConfigurator.configure("log4j2.xml");
+
+
+
         try(InputStream stream = new FileInputStream(args[0])) {
             Properties prop = new Properties();
             prop.load(stream);
+//            var tti = new TTI(prop.getProperty("commandJson"));
+//            var intent = tti.ParseTextToCommand("play music");
+//            // check that intent was parsed
+//            if(intent != null) {
+//                System.out.printf("Intent\n%s\n", intent);
+//            }
             App app = new App();
+            app.Run(prop);
 
-            app.Run(prop.getProperty("rhinoPath"),prop.getProperty("porcupinePath"), prop.getProperty("picovoiceKey"));
 
         } catch(IOException e) {
             System.err.printf("Failed to stream the properties file!: %s\n", e);
             System.exit(1);
-        } catch(PicovoiceException | LineUnavailableException e) {
+        }
+        catch(PicovoiceException | LineUnavailableException | PorcupineException e) {
             logger.fatal("Failed to initialize picovoice: exception = {}", e.getMessage());
+        } catch (CheetahException e) {
+            logger.fatal("Failed to initialize cheetah: exception = {}", e.getMessage());
         }
     }
 }
