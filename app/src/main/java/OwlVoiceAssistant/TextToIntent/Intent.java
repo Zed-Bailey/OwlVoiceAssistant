9c7573a206ce58e51852e12157c6ee07d7b0d33c
Zed <emailzed03@gmail.com>
2022-06-17 17:22:37 +1000

    remove unnecessary variable

--- app/src/main/java/OwlVoiceAssistant/TextToIntent/Intent.java
+++ app/src/main/java/OwlVoiceAssistant/TextToIntent/Intent.java
@@ -7,8 +7,6 @@ public class Intent {
     public String intent;
     public HashMap<String, String> slots = new HashMap<>();
 
-    public String mapping;
-
     @Override
     public String toString() {
         return String.format("intent: %s\nslots: %s", this.intent, this.slots);
