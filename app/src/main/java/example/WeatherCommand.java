c60fb2ef19a23893352115bce2c65798c6891eff
Zed <emailzed03@gmail.com>
2022-07-04 11:23:16 +1000

    An example implementation

---
+++ app/src/main/java/example/WeatherCommand.java
@@ -0,0 +1,25 @@
+package example;
+
+import OwlVoiceAssistant.CommandInterface;
+import OwlVoiceAssistant.TextToIntent.Intent;
+
+import java.util.Properties;
+import java.util.Random;
+
+public class WeatherCommand implements CommandInterface {
+
+    public WeatherCommand(Properties prop) {
+        // use properties file to get weather api key
+        //....
+    }
+
+    @Override
+    public String ExecuteCommand(Intent intent) {
+        var location = intent.slots.get("location");
+        //... Fetch weather data for location
+        var weatherConditions = new String[]{"sunny", "windy", "raining", "stormy", "hailing"};
+        var random = new Random();
+        var randCondition = random.nextInt(weatherConditions.length);
+        return String.format("The weather is %s in %s", randCondition, location);
+    }
+}
